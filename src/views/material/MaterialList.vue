cfbcdcd7d807c2222c67ebfc14ecf222ce37c3c2
lanvce <1569101820@qq.com>
2022-03-21 20:30:07 +0800

    修改新增按钮

--- src/views/material/MaterialList.vue
+++ src/views/material/MaterialList.vue
@@ -58,7 +58,9 @@
         </div>
         <!-- 操作按钮区域 -->
         <div class="table-operator"  style="margin-top: 5px">
-          <a-button v-if="btnEnableList.indexOf(1)>-1" @click="handleAdd" type="primary" icon="plus">新增</a-button>
+<!--          <a-button v-if="btnEnableList.indexOf(1)>-1" @click="handleAdd" type="primary" icon="plus">新增</a-button>-->
+
+          <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
           <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
             <a-popover title="表格模板">
               <template slot="content">
